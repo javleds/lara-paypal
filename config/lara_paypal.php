996c9d56dc5825427ee6fa926715847b58921821
Javier Ledezma <juls0593@gmail.com>
2020-03-11 23:57:11 -0600

    remove example paypal sandbox keys

--- config/lara_paypal.php
+++ config/lara_paypal.php
@@ -5,8 +5,8 @@ return [
 
     'sandbox' => [
         'url' => 'https://api.sandbox.paypal.com',
-        'client_id' => 'AZgtEi9199wVDmvD8-2mGryP-Z-wNj1_-FDW6ETvmMZFs7_7mhIJXhXCnYGscicOZlsFPqyUPKxXjqH2',
-        'secret' => 'EJxGy-eWAB9Vz1Mx9h7HnSwS4E7joydsX8i8-xU553ggGJLHIr-GZJ2-QwYl-gEj18uyte9NQaospo4A',
+        'client_id' => '',
+        'secret' => '',
     ],
 
     'production' => [
