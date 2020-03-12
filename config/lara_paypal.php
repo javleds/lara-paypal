a3d182d639589f13268041cdb482eb27e4d2f83c
Javier Ledezma <jledezma@medtrainer.com>
2020-03-11 20:45:19 -0600

    Add basis for testing

---
+++ config/lara_paypal.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'sandbox' => [
+        'url' => 'https://api.sandbox.paypal.com',
+    ],
+
+    'production' => [
+        'url' => 'https://api.paypal.com',
+    ]
+];
---
+++ tests/Unit/ConfigTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Javleds\LaraPayPal\Tests\Unit;
+
+use Javleds\LaraPayPal\Tests\BaseTestCase;
+
+class ConfigTest extends BaseTestCase
+{
+    public function testConfigHasSandboxEnvironment(): void
+    {
+        $this->assertTrue(true);
+    }
+}
