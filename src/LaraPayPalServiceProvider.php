4526ccac9c94ec2bc41749abbfbdf74bcd174c46
Javier Ledezma <jledezma@medtrainer.com>
2020-03-11 21:09:28 -0600

    Add required config keys

--- src/LaraPayPalServiceProvider.php
+++ src/LaraPayPalServiceProvider.php
@@ -8,11 +8,19 @@ class LaraPayPalServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-
+        $this->publishes(
+            [
+                getBaseDir('config/lara_paypal.php') => config_path('lara_paypal.php')
+            ],
+            'lara-paypal-config'
+        );
     }
 
     public function register(): void
     {
-
+        $this->mergeConfigFrom(
+            getBaseDir('config/lara_paypal.php'),
+            'lara_paypal'
+        );
     }
 }
--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -6,8 +6,24 @@ use Javleds\LaraPayPal\Tests\BaseTestCase;
 
 class ConfigTest extends BaseTestCase
 {
-    public function testConfigHasSandboxEnvironment(): void
+
+    /**
+     * @dataProvider getRequiredConfigKeysProvider
+     */
+    public function testConfigHasSandboxEnvironment($configKey): void
     {
-        $this->assertTrue(true);
+        $this->assertNotNull(
+            config($configKey)
+        );
+    }
+
+    public function getRequiredConfigKeysProvider(): array
+    {
+        return [
+            ['lara_paypal.sandbox'],
+            ['lara_paypal.sandbox.url'],
+            ['lara_paypal.production'],
+            ['lara_paypal.production.url'],
+        ];
     }
 }
